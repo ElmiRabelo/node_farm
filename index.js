bd4c15cbe698658e42b6935b5b2dbbf5370e8b8f
Elmi Rabelo <rabeloelmi@gmail.com>
2019-10-16 23:53:53 -0300

    Modificações na leitura de arquivos html para criação de templates

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-	const pathName = req.url;
-	
-	if( pathName === '/' || pathName === '/overview') {
-		res.end("Está é a pagina de Overview");
-	} else if(pathName === '/projects') {
-		res.end('Está é a pagina de Projetos')
-	} else {
-		res.writeHead(404, {
-			'Content-type': 'text/html'
-		});
-		res.end('<h1>Page not Found</h1>');
-	}
-	
+  const pathName = req.url;
+
+  //overview page
+  if (pathName === "/" || pathName === "/overview") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join("");
+
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+
+    //product page
+  } else if (pathName === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(tempProduct);
+
+    //API
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    //not found
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html"
+    });
+    res.end("<h1>Not Found</h1>");
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => console.log('Atento a porta 8000'));
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Servidor inicializado");
+});
